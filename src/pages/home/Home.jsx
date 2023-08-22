7d9e2462de51d6057bc4b37ecc82611b565aeefa
Muhammadali06dev <muhammadalideveloper2006@gmail.com>
2023-08-22 21:10:34 +0500

    add hero section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,23 @@
 import "./Home.css"
-
-import React from 'react'
+import { NavLink } from "react-router-dom"
 
 function Home() {
    return (
-      <div>Home</div>
+      <section className="home">
+         <div className="home__container container">
+            <div className="home__info">
+               <p className="home__subtitle">new product</p>
+               <h1 className="home__title">XX99 Mark II
+                  HeadphoneS
+               </h1>
+               <p className="home__description">
+                  Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.
+               </p>
+               <NavLink className="home__btn">see product</NavLink>
+            </div>
+            <div className="home__img"></div>
+         </div>
+      </section>
    )
 }
 
